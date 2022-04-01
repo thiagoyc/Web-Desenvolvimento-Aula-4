68682b3ad3333a4019a9f2ed1dca7bef556c09ff
Thiago Couto <thiagoyc@usp.br>
2022-03-31 21:02:35 -0300

    Html sendo enviado para o server pelo response.send :sparkles:

--- src/index.js
+++ src/index.js
@@ -11,8 +11,10 @@ server.get('/', (request, response) => {
 server.get('/Exercicio', (request, response) => {
   console.log('Hello web');
 
-  response.send(index.html);
+  response.send('<p> <html><head><title> Exercicio </title><style>button {border-color: red;border-width: 1px;border-radius: 10px;background-color: tomato;font-weight: bold;padding: 10px 15px;}button:hover {background-color: aqua;}</style></head><body><div><h1> meu texto bonito </h1><h2> meu texto bonito </h2><h3> meu texto bonito </h3><h4> meu texto bonito </h4><h5> meu texto bonito </h5><p>paragrafo 1</p><p>paragrafo 2</p></div><div><form><input /><input /><input /><input /><button> Meu botão </button></form></div><script>console.log("hello web");</script></body> </p>');
 });
+
+// const html = '<html><head><title> Exercicio </title><style>button {border-color: red;border-width: 1px;border-radius: 10px;background-color: tomato;font-weight: bold;padding: 10px 15px;}button:hover {background-color: aqua;}</style></head><body><div><h1> meu texto bonito </h1><h2> meu texto bonito </h2><h3> meu texto bonito </h3><h4> meu texto bonito </h4><h5> meu texto bonito </h5><p>paragrafo 1</p><p>paragrafo 2</p></div><div><form><input /><input /><input /><input /><button> Meu botão </button></form></div><script>console.log("hello web");</script></body>';
 
 server.listen(3000, () =>{
   console.log('Estou pronto');
